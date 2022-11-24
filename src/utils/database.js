929c56748f412479b2d0340e7d2ae875e887c74c
FlorencioQuelca <florens.lonwe@gmail.com>
2022-11-23 22:06:12 -0400

    conexion database

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,5 +2,20 @@ const { Sequelize } = requier('sequelize')
 
 
 const db = new Sequelize({
-    dialect: 'postgres'
+    dialect: 'postgres',
+    host: "localhost",
+    database: "todo-db",
+    username: "postgress",
+    password: "root",
+    port: 5432
 })
+
+db.authenticate()
+    .then(() => console.log("database authenticada correctamente"))
+    .catch(e => console.log(e))
+
+db.sync()
+    .then(() => console.log("database sincronizado correctamente"))
+    .catch(e => console.log(e))
+
+module.exports = db
